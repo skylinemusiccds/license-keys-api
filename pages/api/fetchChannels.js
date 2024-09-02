91063d3b04f375d71710b9830286ebf205d7850a
Playflix007 <pflix41@gmail.com>
2024-09-02 14:07:32 +0530

    added fetchChannels.js

--- pages/api/fetchChannels.js
+++ pages/api/fetchChannels.js
@@ -15,8 +15,9 @@ export default async function handler(req, res) {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
 
-        const channels = await response.json('https://babel-in.xyz/babel-b2ef9ad8f0d432962d47009b24dee465/tata/channels');
+        const channels = await response.json();
 
+        // Map the channel data to the desired format
         const channelData = channels.map(channel => ({
             id: channel.id,
             name: channel.name,
@@ -34,7 +35,23 @@ export default async function handler(req, res) {
             clearkey: channel.clearkeys ? JSON.stringify(channel.clearkeys[0].base64) : null
         }));
 
-        res.status(200).json(channelData);
+        // Generate the M3U string
+        let m3uStr = "#EXTM3U\n\n";
+
+        channelData.forEach(channel => {
+            m3uStr += '#EXTINF:-1 tvg-id="' + channel.id.toString() + '" ';
+            m3uStr += 'group-title="' + (channel.group_title || '') + '", ';
+            m3uStr += 'tvg-logo="https://mediaready.videoready.tv/tatasky-epg/image/fetch/f_auto,fl_lossy,q_auto,h_250,w_250/' + (channel.tvg_logo || '') + '", ';
+            m3uStr += channel.name + '\n';
+            m3uStr += '#KODIPROP:inputstream.adaptive.license_type=clearkey\n';
+            m3uStr += '#KODIPROP:inputstream.adaptive.license_key=' + (channel.clearkey || '') + '\n';
+            m3uStr += '#EXTVLCOPT:http-user-agent=' + (channel.stream_headers || '') + '\n';
+            m3uStr += channel.stream_url + '\n\n';
+        });
+
+        // Set the response type to plain text for M3U
+        res.setHeader('Content-Type', 'text/plain');
+        res.status(200).send(m3uStr);
     } catch (error) {
         console.error('Error fetching channel data:', error);
         res.status(500).json({ error: 'Failed to fetch channel data' });
